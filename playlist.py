8e90134dff9386ea142e95ecc241c774531ff89d
Denilton Nunes <67246260+DeniltoNunes@users.noreply.github.com>
2023-12-02 19:50:06 -0300

    Criar Playlist.
    Criação e debug para a criação de playlists.

--- playlist.py
+++ playlist.py
@@ -10,26 +10,8 @@ nomes_das_musicas = [
 ]
 
 id_musicas = []
-
-# Para cada música na lista, busca o ID correspondente
-for nome_da_musica in nomes_das_musicas:
-    # Realiza a busca na API do Spotify
-    results = sp.search(q=nome_da_musica, limit=1, type='track')
-
-    # Obtém o ID da primeira música encontrada na busca
-    if results['tracks']['items']:
-        track_id = results['tracks']['items'][0]['id']
-        id_musicas.append(track_id)
-    else:
-        print(f"Música '{nome_da_musica}' não encontrada.")
-
-# Criar uma nova playlist
-playlist = sp.user_playlist_create(user=idUsuario, name= nome_playlist, public=False)
-
-# Adicionar músicas à playlist
-sp.user_playlist_add_tracks(user=idUsuario, playlist_id=playlist['id'], tracks=id_musicas)
-
-
+idUsuario = ''
+nome_playlist = ''
 
 def login():
     print("Olá, tudo bem?")
@@ -57,6 +39,7 @@ def id():
     autorizacao = str(input('Você sabe o seu Id do Perfil?\n')).upper()
     while validacao == False:
         if (autorizacao == 'SIM'):
+            global idUsuario
             idUsuario = input('Qual o seu Id de Usuário?\n')
             validacao = True
             criacao()
@@ -74,6 +57,7 @@ def id():
 def criacao():
     validacao = False
     print('Ok, iremos começar a criar a sua Playlist')
+    global nome_playlist
     nome_playlist = input(str("Qual nome você gostaria de dar para a sua Playlist?\n"))
     print('Muito obrigado!')
     autorizacao = str(input('Você já adicionou suas músicas?\n')).upper()
@@ -92,10 +76,33 @@ def criacao():
                 autorizacao = str(input('Você já adicionou suas músicas?\n')).upper()
             validacao = False
             
-def musicas ():
+def musicas():
     print('Pode adicionar quantas músicas você quiser, basta apenas me dizer o nome.')
     musica = str(input('Qual música você quer adicionar? \nCaso não queira mais adicionar digite "Seguinte."'))
     while musica.upper() != 'SEGUINTE':
-        nome_da_musica.append(musica)
+        nomes_das_musicas.append(musica)
         musica = str(input('Qual a proxima música você quer adicionar? \nCaso não queira mais adicionar digite "Seguinte."'))
+    playlist()
+
+def playlist():
+    if len(nomes_das_musicas) == 0:
+        print('Músicas insuficientes.')
+        musicas()
+    else:
+        # Para cada música na lista, busca o ID correspondente
+        for nome_da_musica in nomes_das_musicas:
+            # Realiza a busca na API do Spotify
+            results = sp.search(q=nome_da_musica, limit=1, type='track')
+
+            # Obtém o ID da primeira música encontrada na busca
+            if results['tracks']['items']:
+                track_id = results['tracks']['items'][0]['id']
+                id_musicas.append(track_id)
+            else:
+                print(f"Música '{nome_da_musica}' não encontrada.")
+        # Criar uma nova playlist
+    playlist = sp.user_playlist_create(user=idUsuario, name=nome_playlist, public=False)
+    # Adicionar músicas à playlist
+    sp.user_playlist_add_tracks(user=idUsuario, playlist_id=playlist['id'], tracks=id_musicas)
+            
 login()
